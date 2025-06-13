3f070371207cfd7244208bebc5ff7f55d746c79a
praveen <venkatapraveenkumarmtech@gmail>
2025-06-13 09:48:21 +0530

    Add project files.

---
+++ step1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+
+     class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("hi praveen now you are create one program by using c#");
+        Console.WriteLine("now you know all topics");
+        string value= Console.ReadLine();
+        Console.WriteLine(value);
+        readmessages();
+
+
+        
+         }
+    static void readmessages()
+    {
+        Console.WriteLine("read messages");
+    }
+    }
+
