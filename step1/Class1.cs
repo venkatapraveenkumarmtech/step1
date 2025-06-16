2a08137c8229a5536059433e714d5cb3e702b562
praveen <venkatapraveenkumarmtech@gmail>
2025-06-16 22:28:23 +0530

    datatypesc#

---
+++ step1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+
+     class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to todays session");
+        byte age = 20;
+        Console.WriteLine("byte value: "+age);
+        short temp = -20;
+        Console.WriteLine("short value: "+temp);
+        
+        ushort population = 50000;
+        Console.WriteLine("ushort value: " + population);
+        int companies = 200000;
+        Console.WriteLine("int value"+companies);
+
+        }
+    }
+
